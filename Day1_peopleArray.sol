b21a1ac6adb7d9e8f4513b96c48ded7edf21923d
Matt Pilchard <iamchaoticcoder@gmail.com>
2020-07-25 19:40:21 -0500

    returns args issue

--- Day1_peopleArray.sol
+++ Day1_peopleArray.sol
@@ -3,27 +3,32 @@ pragma solidity 0.5.12;
 contract HelloWorld{
 // MNP ChaoticCoder
     // state variables //
-    // create the struct for a person (index, name, age, height, senior)
+    // create the struct for a person (people, name, age, height, senior)
     struct Person {
       uint id;
       string name;
       uint age;
       uint height;
       bool senior;
+      address Creator;
     }
     
     Person [] public people; // array of people
     
     // functions
 
+    function getPerson(uint index) public  {
+        //- Create a public get function where we can input an people and retrieve the Person object with that people in the array.
+        // compiler does not like the number of args I have.
+        return (people[index].name, people[index].age, people[index].height, people[index].senior, people[index].Creator); // pull info for supplied index
+    }
 
-    function createPerson(string memory name, uint age, uint height, bool senior) public {
-   
+// Modify the Person struct and  add an address property Creator. Make sure to edit the createPerson function so that it sets this property to the msg.sender.
+    function createPerson(string memory name, uint age, uint height, bool senior, address creator) public {
         Person memory newPerson;
         newPerson.name = name;
         newPerson.age = age;
         newPerson.height = height;
-        
         // determine senior status
         if(age >= 65){
            newPerson.senior = true;
@@ -31,7 +36,9 @@ contract HelloWorld{
         else{
            newPerson.senior = false;
         }
-        
+        newPerson.Creator = msg.sender;
+           
+        //When someone creates a new person, add the Person object to the people array instead of the mapping.
         people.push(newPerson); // add new person to the arrray 
     }
 }
